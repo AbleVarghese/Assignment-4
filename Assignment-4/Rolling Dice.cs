7ea3db6bf4932631ef09087904f451983c78bcea
Able Varghese <able.varghese12@gmail.com>
2015-03-17 01:00:47 -0400

    Class created for Rolling dices

---
+++ Assignment-4/Rolling Dice.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_4
+{
+    class Rolling_Dice
+    {
+        //Instance Variables
+
+        int[] dice1 = new int[6];
+        int[] dice2 = new int[6];
+
+
+        //Properties
+
+       void RollingDice1()
+        {
+           Random dice1 = new Random();
+
+           for (int index = 0; index < 10; index++)
+           {
+               dice1.Next(1, 6);
+           }
+           
+
+
+        }
+
+
+
+
+    }
+}
