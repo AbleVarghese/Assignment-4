f8556c8141591e7f5ee8d545c1aa6c2e11bc19b5
Able Varghese <able.varghese12@gmail.com>
2015-03-19 11:48:12 -0400

    Formatted and Intented properly

--- Assignment-4/Rolling Dice.cs
+++ Assignment-4/Rolling Dice.cs
@@ -16,9 +16,7 @@ namespace Assignment_4
         public int[] dice2Rolls = new int[36000];
         public int[] sum = new int[36000];
         public int[] repeatedSums = new int[11];
-        public int[] possibleSums = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
-        
-        
+        public int[] possibleSums = { 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
 
         //Properties
 
@@ -39,93 +37,76 @@ namespace Assignment_4
             }
         }
 
-      public void RollingDice2()
-      {
-        
-          Random dice2 = new Random();
+        public void RollingDice2()
+        {
 
-          for (int index = 0; index < 36000; index++)
-          {
-              this.dice2Rolls[index] = dice2.Next(1, 7);
-              if (this.dice2Rolls[index] < 4)
-              {
-                  this.dice2Rolls[index] = dice2.Next(1, 4);
-              }
-            //  Console.Write("{0},  ", dice2Rolls[index]);
+            Random dice2 = new Random();
 
-          }
-          Console.Write("....\n\n\n");
-      }
+            for (int index = 0; index < 36000; index++)
+            {
+                this.dice2Rolls[index] = dice2.Next(1, 7);
+                if (this.dice2Rolls[index] < 4)
+                {
+                    this.dice2Rolls[index] = dice2.Next(1, 4);
+                }
 
 
+            }
+
+        }
+
+        public void Sums()
+        {
+
+            for (int index = 0; index < 36000; index++)
+            {
+
+                this.sum[index] = this.dice1Rolls[index] + this.dice2Rolls[index];
+
+            }
+
+        }
 
 
+        public void Compare()
+        {
 
+            for (int index = 0; index < 11; index++)
+            {
 
-      public void Sums()
-      {
+                for (int i = 0; i < 36000; i++)
+                {
+                    if (this.sum[i] == this.possibleSums[index])
 
-           for (int index = 0; index <36000; index++)
-           {
-             
-               this.sum[index] = this.dice1Rolls[index] + this.dice2Rolls[index];
+                    { this.repeatedSums[index] += 1; }
 
-              // Console.Write("{0},  ", sum[index]);
+                    
+                }
 
-           }
-          // Console.Write("....\n\n\n");
-       }
-
-
-
-
-
-       public void Compare()
-       {
-
-
-          //Clear(repeatedSums, 0, repeatedSums.Length);
-          for (int index = 0; index < 11; index++)
-           {
-            
-               for (int i = 0; i < 36000; i++)
-               {
-                   if (this.sum[i] == this.possibleSums[index])
-                   {
-                       //Console.WriteLine("--jnjnjnj");
-                       this.repeatedSums[index] += 1;
-
-                   }
-               }
-
-           }
-       }
- 
+            }
+        }
 
 
         public void Display()
         {
-                Console.WriteLine("1st Dice Roll\t2nd Dice Roll\tSums\n");
-            for (int index = 0; index < 5; index++)
+            Console.WriteLine("1st Dice Roll\t2nd Dice Roll\tSums\n");
+            for (int index = 0; index < 4; index++)
             {
-                
+
                 Console.WriteLine("    {0}         +        {1}       \t= {2}", this.dice1Rolls[index], this.dice2Rolls[index], sum[index]);
                 Console.WriteLine("------------------------------------------");
 
             }
-            Console.WriteLine("etc.. etc...  Above table continued upto 36000 times");
+            Console.WriteLine("etc.. etc...  Above table is continued upto 36000 Rolls of both dices");
             Console.WriteLine("\n\n   Possible Sums\t  No. of Occurance\n");
             for (int i = 0; i < 11; i++)
             {
-                    Console.WriteLine("\t{0}\t\t\t{1}", possibleSums[i], repeatedSums[i]);
+                Console.WriteLine("\t{0}\t\t\t{1}", possibleSums[i], repeatedSums[i]);
 
             }
             Console.Write("\n\n");
-        
-        }
-                   
-              
 
+        }
 
     }
 }
