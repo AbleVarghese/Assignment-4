5936e849c844f0e0524e19c72d4f57157b6355a3
Able Varghese <able.varghese12@gmail.com>
2015-03-20 10:33:55 -0400

    Main Menu is made to select between Apps.

--- Assignment-4/Program.cs
+++ Assignment-4/Program.cs
@@ -10,7 +10,13 @@ namespace Assignment_4
     {
         static void Main(string[] args)
         {
-/*
+
+ Console.WriteLine("Please make your choice between Applications. \n\n1. Rolling Dice App. \n2. Airline Reservation System\n");
+ int select=Convert.ToInt32(Console.ReadLine());
+ 
+            if(select==1)
+            {
+
             Rolling_Dice Rolling = new Rolling_Dice();
             Rolling.RollingDice1();
             Rolling.RollingDice2();
@@ -18,11 +24,16 @@ namespace Assignment_4
             Rolling.Compare();
             Rolling.Display();
             Console.ReadKey();
- * 
- */
-            Airline_Reservation_System Flight = new Airline_Reservation_System();
-            Flight.Menu();
-            
+            }
+
+            else if (select == 2)
+            {
+                Airline_Reservation_System Flight = new Airline_Reservation_System();
+                Flight.Menu();
+            }
+
+            else
+            { Console.WriteLine("\nPlease enter choice No. 1 or choice No. 2"); }
 
 
 
