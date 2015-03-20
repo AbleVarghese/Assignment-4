03f02a78f534c9207e25cb40ffbda86acbc8d24d
Able Varghese <able.varghese12@gmail.com>
2015-03-20 10:09:05 -0400

    Airline reservation system(functionally complete) intended and formatted.

--- Assignment-4/Airline Reservation System.cs
+++ Assignment-4/Airline Reservation System.cs
@@ -15,43 +15,43 @@ namespace Assignment_4
         public void Menu()
         {
             int choice = 0;
-           
-            int counter=0;
+
+            int counter = 0;
 
             for (int index = 0; index < 5; index++)
             {
                 firstClassSeats[index] = false;
                 economySeats[index] = false;
             }
-            
+
 
 
             while (choice != 3)
             {
-                counter+=1;
-                Console.WriteLine("1. First Class\n2. Economy\n3. Exit\n");
+                counter += 1;
+                Console.WriteLine("\n1. First Class\n2. Economy\n3. Exit\n");
                 choice = Convert.ToInt32(Console.ReadLine());
-               // if (firstClassSeatNum >= 6)
-               //     System.Environment.Exit(0);
+                // if (firstClassSeatNum >= 6)
+                //     System.Environment.Exit(0);
                 //else if (economySeatNum >= 6)
                 //    System.Environment.Exit(0);
 
                 switch (choice)
                 {
-                    case 1: 
+                    case 1:
                         FirstClassSeatReservation();
-                        
+
 
                         break;
                     case 2: EconomySeatReservation();
-                        
+
 
                         break;
 
-                    case 3: Console.WriteLine( "Exiting the system");
+                    case 3: Console.WriteLine("\nExiting the system");
                         break;
 
-                    default: Console.WriteLine("Please make a selection..");
+                    default: Console.WriteLine("\nPlease make a selection..\n");
                         break;
 
                 }
@@ -59,78 +59,130 @@ namespace Assignment_4
         }
 
         public void EconomySeatReservation()
-
-
+        {
+            int indexSeatNo = 6;
+            int index2 = 0;
+            while (index2 < 5)
+            {
+                // Console.WriteLine("{0}a",indexSeatNo);
+                if (economySeats[index2] == false)
                 {
-                    int indexSeatNo = 6;
-                    int index2 = 0;
-                    while (index2 < 5)
-                    {
-                       // Console.WriteLine("{0}a",indexSeatNo);
-                        if (economySeats[index2] == false)
-                        {
-                            economySeats[index2] = true;
-                            Console.WriteLine("You have successfully reserved economy class ticket, your seat number is {0}", (indexSeatNo));
-                            return;
-                        }
-                        index2 += 1;
-                        indexSeatNo += 1;
-                        
-
-                    }
-                    Console.WriteLine("sorry, economy class has no more seats left\n\n Would you like to reserve seat in first class? please type in yes/no ");
-                    string changeClass = Console.ReadLine();
-                    if (changeClass == "yes" || changeClass== "YES")
-                    {
-                        FirstClassSeatReservation();
- 
-                    }
-                    if (changeClass == "no" || changeClass == "NO")
-                    {
-                        return;
-
-                    }
-                    
-
-                }
-
-
-
-                public void FirstClassSeatReservation()
-
-                {
-                    int index = 1;
-                    int loopIndex = 0;
-                    while (loopIndex < 5)
-                    {
-                        if (firstClassSeats[loopIndex] == false)
-                        {
-                            firstClassSeats[loopIndex] = true;
-                            Console.WriteLine("You have successfully reserved first class ticket, your seat number is {0}", (index));
-                            return;
-                        }
-                        loopIndex += 1;
-                        index += 1;
-
-
-                    }
-                    Console.WriteLine("sorry, first class has no more seats left\n\n Would you like to reserve seat in economy class? ");
-                    string changeClass = Console.ReadLine();
-                    if (changeClass == "yes" || changeClass == "YES")
-                    {
-                        EconomySeatReservation();
-
-                    }
-                    if (changeClass == "no" || changeClass == "NO")
-                    {
-                        return;
-
-                    }
-                    
-
+                    economySeats[index2] = true;
+                    Console.Write("You have successfully reserved economy class ticket, your seat number is {0}", (indexSeatNo));
                     return;
-                    
                 }
+                index2 += 1;
+                indexSeatNo += 1;
+
+            }
+            Console.WriteLine("Sorry, economy class has no more seats left.\n\n");
+
+            int x = SeatsFullMethod();
+
+            if (x == 5)
+            {
+                Console.WriteLine("Sorry, the seats are full, Next flight is in 3 hours");
+                return;
+            }
+            else
+            {
+
+                Console.Write(" Would you like to reserve seat in first class? please type in yes/no: ");
+                string changeClass = Console.ReadLine();
+                if (changeClass == "yes" || changeClass == "YES")
+                {
+                    FirstClassSeatReservation();
+
+                }
+                if (changeClass == "no" || changeClass == "NO")
+                {
+                    return;
+
+                }
+            }
+
+
+        }
+
+
+
+        public void FirstClassSeatReservation()
+        {
+            int index = 1;
+            int loopIndex = 0;
+            while (loopIndex < 5)
+            {
+                if (firstClassSeats[loopIndex] == false)
+                {
+                    firstClassSeats[loopIndex] = true;
+                    Console.WriteLine("You have successfully reserved first class ticket, your seat number is {0}", (index));
+                    return;
+                }
+                loopIndex += 1;
+                index += 1;
+
+
+            }
+
+
+            Console.WriteLine("sorry2, first class has no more seats left\n\n");
+
+            int seatFullIndex = SeatsFullMethod();
+
+
+            if (seatFullIndex == 5)
+            {
+                Console.WriteLine("Sorry, the seats are full, Next flight is in 3 hours");
+                return;
+            }
+
+            Console.WriteLine("Would you like to reserve seat in economy class? ");
+
+            string changeClass = Console.ReadLine();
+            if (changeClass == "yes" || changeClass == "YES")
+            {
+                EconomySeatReservation();
+
+            }
+            if (changeClass == "no" || changeClass == "NO")
+            {
+                return;
+
+            }
+
+
+            return;
+
+        }
+
+
+
+        public int SeatsFullMethod()
+        {
+
+
+            int seatFullIndex = 0;
+            while (seatFullIndex < 5)
+            {
+
+                if (firstClassSeats[seatFullIndex] == true && economySeats[seatFullIndex] == true)
+                {
+                    seatFullIndex += 1;
+
+
+
+                }
+                else
+                {
+                    return (seatFullIndex);
+
+                }
+
+            }
+
+            return (seatFullIndex);
+
+        }
 
 
 
